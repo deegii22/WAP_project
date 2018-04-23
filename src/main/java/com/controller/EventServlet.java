97b49b02cbf0735b9b4c6db47f20cfbab75ff1d4
Enkhee <3nkh33@gmail.com>
2018-04-23 16:14:10 -0500

    add eventList

--- src/main/java/com/controller/EventServlet.java
+++ src/main/java/com/controller/EventServlet.java
@@ -3,6 +3,7 @@ package com.controller;
 import com.model.Event;
 import com.service.DBService;
 import com.service.Result;
+import org.json.simple.JSONObject;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -12,9 +13,10 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
 import java.util.Date;
 
-@WebServlet(name = "EventServlet")
+@WebServlet("/Event")
 public class EventServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         PrintWriter out = response.getWriter();
@@ -24,16 +26,29 @@ public class EventServlet extends HttpServlet {
                 case "add":
                     Result res = addEvent(request);
                     out.print(res.getDesc());
+                    addEvent(request);
+                    break;
+                default:
                     break;
             }
-        }
-        catch (Exception ex) {
+        } catch (Exception ex) {
             out.print("Sdarara");
         }
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        String action = request.getParameter("action");
+        PrintWriter out = response.getWriter();
+        switch (action) {
+            case "upcomingList":
+                out.print(eventList(0));
+                break;
+            case "ongoingList":
+                out.print(eventList(1));
+                break;
+            default:
+                break;
+        }
     }
     private Result addEvent(HttpServletRequest request)
     {
@@ -61,5 +76,10 @@ public class EventServlet extends HttpServlet {
         catch (Exception ex){
             return new Result(ex.getMessage(), null);
         }
+    }
+
+    private String eventList(int type) {
+        DBService db = new DBService();
+        return db.eventList(type);
     }
 }
--- src/main/java/com/service/DBService.java
+++ src/main/java/com/service/DBService.java
@@ -1,10 +1,13 @@
 package com.service;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.reflect.TypeToken;
 import com.model.Event;
+import java.sql.*;
+import java.util.*;
 
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.sql.Statement;
 
 public class DBService {
 
@@ -29,7 +32,7 @@ public class DBService {
     public Result AddEvent(Event event) {
         conn = connectDB();
         sql = "insert into Event (name, status, start_date, end_date, route_img, owner_id, emergency_flag, emergency_info, created)\n" +
-                "values ('"+event.getName()+"', 0, STR_TO_DATE('25-04-2018', '%d-%m-%Y'), STR_TO_DATE('26-04-2018', '%d-%m-%Y'), '', 1, 0, '', sysdate());";
+                "values ('" + event.getName() + "', 0, STR_TO_DATE('25-04-2018', '%d-%m-%Y'), STR_TO_DATE('26-04-2018', '%d-%m-%Y'), '', 1, 0, '', sysdate());";
 
         try {
 
@@ -41,6 +44,41 @@ public class DBService {
             return new Result(e.getMessage(), null);
         }
         return null;
+    }
+
+    public String eventList(int type) {
+        conn = connectDB();
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+        String ret = "[]";
+        try {
+            preparedStatement = conn.prepareStatement("select * from Event where status = ?");
+            preparedStatement.setInt(1, type);
+            resultSet = preparedStatement.executeQuery();
+            List<Event> events = new ArrayList();
+            while (resultSet.next()) {
+                Event event = new Event();
+                event.setEventID(resultSet.getInt("event_id"));
+                event.setName(resultSet.getString("name"));
+                event.setStatus(resultSet.getInt("status"));
+                event.setStartDate(resultSet.getDate("start_date"));
+                event.setEndDate(resultSet.getDate("end_date"));
+                event.setImg(resultSet.getString("route_img"));
+                event.seteFlag(resultSet.getInt("emergency_flag"));
+                event.seteInfo(resultSet.getString("emergency_info"));
+                event.setCreated(resultSet.getDate("created"));
+                events.add(event);
+            }
+            //https://github.com/google/gson
+            Gson gson = new Gson();
+            JsonElement element = gson.toJsonTree(events, new TypeToken<List<Event>>() {
+            }.getType());
+            JsonArray jsonArray = element.getAsJsonArray();
+            ret = jsonArray.toString();
+        } catch (Exception e) {
+            ret = "[]";
+        }
+        return ret;
     }
         /*
         public Result login(String userId, String password) {
