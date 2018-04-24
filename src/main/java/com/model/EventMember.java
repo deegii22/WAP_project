87699799a1966c453f64de4f2c1c4eeaa2e0bd0f
Enkhee <3nkh33@gmail.com>
2018-04-24 18:21:03 -0500

    added

--- src/main/java/com/model/EventMember.java
+++ src/main/java/com/model/EventMember.java
@@ -5,6 +5,7 @@ import java.io.Serializable;
 public class EventMember implements Serializable {
     private long eventID;
     private int userId;
+    private String userName;
 
     public long getEventID() {
         return eventID;
@@ -20,5 +21,13 @@ public class EventMember implements Serializable {
 
     public void setUserId(int userId) {
         this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
     }
 }
